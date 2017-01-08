1dabeda7de59c7b460137f276337beb9f4730424
Andreas Lagerstedt <AndreasLagerstedt@Andreass-MacBook-Pro-2.local>
2017-01-08 17:36:18 +0100

    A lot of fucking around with creating some kind og player character

---
+++ Assets/Script/Cubefollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubefollow : MonoBehaviour {
+
+	GameObject instance;
+	public GameObject character;
+	// Use this for initialization
+	void Start () {
+		instance = this.gameObject;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		instance.transform.position = character.transform.position;
+	}
+}
---
+++ Assets/Script/SpriteRandomizer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRandomizer : MonoBehaviour {
+
+	public Material[] mats;
+	public int select = 0;
+	int dir = 1;
+	float timer = 1;
+	MeshRenderer r;
+
+	void Start () {
+		r = GetComponent<MeshRenderer> ();
+		
+	}
+	
+
+	void Update () {
+
+		if (Input.GetKey(KeyCode.Space)){
+			timer += 0.3f;
+
+			if (timer >= 1){
+				timer = 0f;
+				if(select >= mats.Length-1){
+					//select = 0;
+					dir *= -1;
+				}
+				else if (select <= 0){
+					select = 1;
+					dir *= -1;
+				}
+
+				select += dir;
+			}
+
+
+
+
+			r.material = mats [select];
+
+		}
+		else{
+			timer = 1;
+			r.material = mats [0];
+		}
+
+	}
+}
