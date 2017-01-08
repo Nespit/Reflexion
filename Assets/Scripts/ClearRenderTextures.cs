80b9fd7375a1c79250e959648fbd60724416b52c
Leonhard Pickny <leonhard.pickny@gmail.com>
2017-01-08 18:35:54 +0100

    Replaced Grid with single tile and fixed the random artifacts.

---
+++ Assets/Scripts/ClearRenderTextures.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearRenderTextures : MonoBehaviour {
+
+	[SerializeField]
+	private RenderTexture tileRenderTexture;
+
+	// Use this for initialization
+	void Start () {
+		RenderTexture.active = tileRenderTexture;
+		GL.Clear(false, true, Color.clear);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnPreRender ()
+	{
+		RenderTexture currentActiveRT;
+
+		currentActiveRT = RenderTexture.active;
+		RenderTexture.active = tileRenderTexture;
+		GL.Clear(false, true, Color.clear);
+		RenderTexture.active = currentActiveRT;
+	}
+}
